78410955b73f38f626686b2d8838fcd8d623d6bc
Lê Hùng Quang Minh <158133523+quangminhlh@users.noreply.github.com>
2025-01-30 13:36:16 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const config = {
     serverInfo: {
         serverLogoImageFileName: "logo.png", /*This is a file name for logo in /images/ (If you upload new logo with other name, you must change this value)*/
         serverName: "Zynther MC", /*Server name*/
-        serverIp: "mc.zynther.xyz", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
+        serverIp: "mc.zynther.xyz:27112", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
         discordServerID: "1293438530202107935" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
     },
 
